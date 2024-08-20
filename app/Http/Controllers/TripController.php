40a333508cf587d5442f48a3f3227fbe6511485b
emilianogastaldo <emiliano0303@gmail.com>
2024-08-20 19:13:33 +0200

    showpage

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -36,9 +36,9 @@ class TripController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Trip $trip)
     {
-        //
+        return inertia('Trips/ShowPage', compact('trip'));
     }
 
     /**
---
+++ resources/js/Pages/Trips/ShowPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    trip: Object,
+});
+
+</script>
+
+<template>
+
+    <Head title="Trips" />
+    <section class="container mx-auto">
+        <h1 class="bg-amber-600">{{ trip.title }}</h1>
+    </section>
+
+</template>
+
+<style></style>
