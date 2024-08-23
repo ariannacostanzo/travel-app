d602cf4a89cab58fe329dfed0487f4fdb9b4d187
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-08-23 17:19:25 +0200

    added footer

---
+++ resources/js/Components/FooterComponent.vue
@@ -0,0 +1,50 @@
+<script setup></script>
+
+<template>
+    <footer class="bg-red-600 flex justify-center items-center">
+        <div>
+            <ul>
+                <li>When do you want to depart?</li>
+                <li>Group trips January</li>
+                <li>Group trips February</li>
+                <li>Group trips March</li>
+                <li>Group trips April</li>
+                <li>Group trips May</li>
+                <li>Group trips June</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>When do you want to depart?</li>
+                <li>Group trips January</li>
+                <li>Group trips February</li>
+                <li>Group trips March</li>
+                <li>Group trips April</li>
+                <li>Group trips May</li>
+                <li>Group trips June</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>When do you want to depart?</li>
+                <li>Group trips January</li>
+                <li>Group trips February</li>
+                <li>Group trips March</li>
+                <li>Group trips April</li>
+                <li>Group trips May</li>
+                <li>Group trips June</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>When do you want to depart?</li>
+                <li>Group trips January</li>
+                <li>Group trips February</li>
+                <li>Group trips March</li>
+                <li>Group trips April</li>
+                <li>Group trips May</li>
+                <li>Group trips June</li>
+            </ul>
+        </div>
+    </footer>
+</template>
--- resources/js/Layouts/GeneralLayuot.vue
+++ resources/js/Layouts/GeneralLayuot.vue
@@ -1,12 +1,13 @@
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
 </script>
 
 <template>
     <HeaderComponent />
     <main>
         <slot />
-
+        <FooterComponent />
     </main>
 </template>
 
