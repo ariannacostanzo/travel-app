c104542ec288bbfa5fb4285ff76a890f76d5850d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-08-23 16:06:02 +0200

    added home page layout

---
+++ resources/js/Components/HeaderComponent.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <header class="h-[100px] bg-red-600 flex justify-center items-center">
+        <h1 class="text-5xl">Header</h1>
+    </header>
+</template>
---
+++ resources/js/Layouts/GeneralLayuot.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeaderComponent from '@/components/HeaderComponent.vue';
+</script>
+
+<template>
+    <HeaderComponent />
+    <main>
+        <slot />
+
+    </main>
+</template>
+
+<style></style>
