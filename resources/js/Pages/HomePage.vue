06d6f1cc836a534779b5a4009ed902cdde0a1bcb
ariannacostanzo <costanzo.arianna@outlook.it>
2024-08-23 17:14:25 +0200

    styled a bit the header

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -48,11 +48,11 @@ const form = useForm({
 
                 <div class="absolute text-center">
 
-                    <h1 class="text-5xl text-white">gioacchino merda</h1>
+                    <h1 class="text-5xl text-white">gioacchino shit</h1>
 
                     <a class="inline-flex items-center cursor-pointer mt-8 px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
                         href="#register">
-                        Registrati
+                        Register
                     </a>
 
                 </div>
