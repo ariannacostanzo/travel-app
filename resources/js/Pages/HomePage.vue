4ba5ca508667fef3efcda4e56b3fde60f71ec333
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-08-25 14:50:33 +0200

    created Home Page

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,10 +1,9 @@
 <script setup>
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
+import { useForm } from 'laravel-precognition-vue-inertia';
 import GeneralLayout from '@/Layouts/GeneralLayuot.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import TextInput from '@/Components/TextInput.vue';
 import image from '../../../public/storage/jumbotron_img/paesaggio.jpg';
+import FormComponent from '@/Components/Form/FormComponent.vue';
 
 defineProps({
     canLogin: {
@@ -23,111 +22,111 @@ defineProps({
     },
 });
 
-const form = useForm({
+// Dati del form di registrazione
+const registerForm = useForm('post', '/register', {
     email: '',
     name: '',
     last_name: '',
     password: '',
+    password_confirmation: '',
 });
+
+// Dati del form di login
+const loginForm = useForm('post', '/login', {
+    email: '',
+    password: '',
+});
+
+// Funzione per il submit del form
+const submit = (option) => {
+
+    if (option === 'Register') {
+
+        registerForm.submit({
+            preserveScroll: true,
+            onSuccess: () => registerForm.reset(),
+        });
+    }
+
+    if (option === 'Login') {
+
+        loginForm.submit({
+            preserveScroll: true,
+            onSuccess: () => loginForm.reset(),
+        });
+    }
+}
+</script>
+
+<script>
+export default {
+    data: () => ({
+        isHidden: true,
+        option: ''
+    })
+}
 </script>
 
 <template>
 
     <Head title="Home" />
 
+    <!-- Layout Generico creato da noi -->
     <GeneralLayout>
 
-        <!-- Sezione del Jumbotron -->
-        <section id="jumbotron">
+        <!-- Sezione Home -->
+        <section id="home" class="h-full relative">
 
-            <div class="h-[700px] flex justify-center items-center relative">
+            <!-- Jumbotron -->
+            <div class="h-full flex justify-center items-center relative">
 
+                <!-- Immagine del Jumbotron -->
                 <figure class="h-full w-full">
                     <img :src="image" alt="Immagine" class="h-full w-full object-cover">
                 </figure>
 
+                <!-- Testo con i due buttoni (apre l'offcanvas della Register) -->
                 <div class="absolute text-center">
 
                     <h1 class="text-5xl text-white">gioacchino shit</h1>
 
-                    <a class="inline-flex items-center cursor-pointer mt-8 px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                        href="#register">
+                    <!-- Bottone per registrarsi -->
+                    <button
+                        class="inline-flex mx-4 items-center cursor-pointer mt-8 px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                        @click="isHidden = !isHidden; option = 'Register'">
                         Register
-                    </a>
+                    </button>
+
+                    <!-- Bottone per fare il login (apre l'offcanvas della Login) -->
+                    <button
+                        class="inline-flex mx-4 items-center cursor-pointer mt-8 px-4 py-2 bg-blue-300 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                        @click="isHidden = !isHidden; option = 'Login'">
+                        Login
+                    </button>
 
                 </div>
 
             </div>
 
-        </section>
+            <!-- Overlay -->
+            <div id="overlay"
+                class="fixed top-0 right-0 bottom-0 left-0 p-8 bg-[#0000007f] transition-opacity duration-200"
+                :class="{ 'invisible': isHidden, 'opacity-0': isHidden }">
 
-        <!-- Sezione del form di registrazione -->
-        <section id="register">
+                <!-- Offcanvas -->
+                <div :class="{ 'translate-x-full': isHidden }"
+                    class="absolute top-0 right-0 bottom-0 bg-slate-200 w-1/2 p-20 transition-all duration-200">
 
-            <div class="container mx-auto my-10">
+                    <!-- Titolo dell'offcanvas -->
+                    <h2 class="text-3xl text-center mb-8">{{ option }}</h2>
 
-                <div class="p-8 bg-white shadow-slate-500 w-10/12 mx-auto rounded-lg">
+                    <!-- Componente del form -->
+                    <FormComponent :option="option" :login="loginForm" :register="registerForm" @submit="submit" />
 
-                    <h2 class="text-3xl text-center mb-8">Register</h2>
-
-                    <!-- Form -->
-                    <form action="">
-                        <div class="columns-2">
-
-                            <!-- Name -->
-                            <div>
-                                <InputLabel class="text-xl" for="name" value="Name" />
-
-                                <TextInput id="name" type="text" class="mt-1 text-lg block h-8 rounded-2xl w-full"
-                                    v-model="form.name" required />
-
-                                <InputError class="mt-2" :message="form.errors.name" />
-                            </div>
-
-
-                            <!-- Email -->
-                            <div class="my-5">
-                                <InputLabel class="text-xl" for="email" value="Email" />
-
-                                <TextInput id="email" type="email" class="mt-1 text-lg h-8 rounded-2xl block w-full"
-                                    v-model="form.email" required />
-
-                                <InputError class="mt-2" :message="form.errors.email" />
-                            </div>
-
-                            <!-- Last Name -->
-                            <div>
-                                <InputLabel class="text-xl" for="last_name" value="Lastname" />
-
-                                <TextInput id="last_name" type="text" class="mt-1 text-lg block h-8 rounded-2xl w-full"
-                                    v-model="form.last_name" required />
-
-                                <InputError class="mt-2" :message="form.errors.last_name" />
-                            </div>
-
-                            <!-- Password -->
-                            <div class="my-5">
-                                <InputLabel class="text-xl" for="password" value="Password" />
-
-                                <TextInput id="password" type="password"
-                                    class="mt-1 text-lg block h-8 rounded-2xl w-full" v-model="form.password"
-                                    required />
-
-                                <InputError class="mt-2" :message="form.errors.password" />
-                            </div>
-
-                        </div>
-
-                        <div class="flex justify-center items-center">
-
-                            <button
-                                class="inline-flex items-center mt-8 px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                Register
-                            </button>
-
-                        </div>
-
-                    </form>
+                    <!-- bottone per chiusura offcanvas -->
+                    <button class="absolute top-5 right-5" @click="isHidden = !isHidden; option = ''">
+                        <font-awesome-icon icon="fas fa-xmark" class="fa-2x" />
+                    </button>
 
                 </div>
 
