05ebdeed96f0110475cdbe19dbf26e7bf2c61773
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-08-25 14:55:15 +0200

    fixed header

--- resources/js/Components/HeaderComponent.vue
+++ resources/js/Components/HeaderComponent.vue
@@ -2,7 +2,7 @@
 import { Link } from '@inertiajs/vue3';</script>
 
 <template>
-    <header class="py-3 px-10 bg-sky-600 flex justify-between items-center">
+    <header class="h-[100px] py-3 px-10 bg-sky-600 flex justify-between items-center">
         <h2>Logo</h2>
         <Link href="/login">
         <h2>Login</h2>
