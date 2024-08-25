d1603ca5b3185a27774f9d4c6ac3898a5f39700f
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-08-25 15:25:31 +0200

    added footer

--- resources/js/Components/FooterComponent.vue
+++ resources/js/Components/FooterComponent.vue
@@ -1,50 +1,49 @@
 <script setup></script>
 
 <template>
-    <footer class="bg-red-600 flex justify-center items-center">
-        <div>
-            <ul>
-                <li>When do you want to depart?</li>
-                <li>Group trips January</li>
-                <li>Group trips February</li>
-                <li>Group trips March</li>
-                <li>Group trips April</li>
-                <li>Group trips May</li>
-                <li>Group trips June</li>
-            </ul>
-        </div>
-        <div>
-            <ul>
-                <li>When do you want to depart?</li>
-                <li>Group trips January</li>
-                <li>Group trips February</li>
-                <li>Group trips March</li>
-                <li>Group trips April</li>
-                <li>Group trips May</li>
-                <li>Group trips June</li>
-            </ul>
-        </div>
-        <div>
-            <ul>
-                <li>When do you want to depart?</li>
-                <li>Group trips January</li>
-                <li>Group trips February</li>
-                <li>Group trips March</li>
-                <li>Group trips April</li>
-                <li>Group trips May</li>
-                <li>Group trips June</li>
-            </ul>
-        </div>
-        <div>
-            <ul>
-                <li>When do you want to depart?</li>
-                <li>Group trips January</li>
-                <li>Group trips February</li>
-                <li>Group trips March</li>
-                <li>Group trips April</li>
-                <li>Group trips May</li>
-                <li>Group trips June</li>
-            </ul>
-        </div>
+    <footer class="bg-[#91635C]">
+        <section class="flex p-20 gap-5 container mx-auto flex-col">
+            <div class="h-[50px] w-[50px] bg-blue-500 flex items-center justify-center">
+                Logo
+            </div>
+            <div class="flex justify-between text-white">
+                <div>
+                    <ul>
+                        <li>
+                            <h3 class="text-xl font-semibold mb-3">When do you want to depart?</h3>
+                        </li>
+                        <li><a href="#">Group trips January</a></li>
+                        <li><a href="#">Group trips February</a></li>
+                        <li><a href="#">Group trips March</a></li>
+                        <li><a href="#">Group trips April</a></li>
+                        <li><a href="#">Group trips May</a></li>
+                        <li>Group trips June</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h3 class="text-xl font-semibold mb-3">Team</h3>
+                        </li>
+                        <li><a href="https://github.com/emilianogastaldo">Emiliano Gastaldo</a></li>
+                        <li><a href="https://github.com/DaniAm94">Danilo Amico</a></li>
+                        <li><a href="https://github.com/ariannacostanzo">Arianna Costanzo</a></li>
+                        <li><a href="https://github.com/SantiGalvan">Santiago Galvan</a></li>
+                        <li><a href="https://github.com/GioacchinoArgo">Gioacchio Argo</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li class="text-xl font-semibold mb-3">When do you want to depart?</li>
+                        <li>Group trips January</li>
+                        <li>Group trips February</li>
+                        <li>Group trips March</li>
+                        <li>Group trips April</li>
+                        <li>Group trips May</li>
+                        <li>Group trips June</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     </footer>
 </template>
--- resources/js/Components/HeaderComponent.vue
+++ resources/js/Components/HeaderComponent.vue
@@ -2,7 +2,7 @@
 import { Link } from '@inertiajs/vue3';</script>
 
 <template>
-    <header class="h-[100px] py-3 px-10 bg-sky-600 flex justify-between items-center">
+    <header class="h-[100px] shadow-lg py-3 px-10 bg-[#F0DEC4] flex justify-between items-center">
         <h2>Logo</h2>
         <Link href="/login">
         <h2>Login</h2>
