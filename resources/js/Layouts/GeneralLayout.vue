6cea38cc0b41626af86708be9c925faa30ce76f0
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-08-26 17:21:36 +0200

    footer completed

--- resources/js/Layouts/GeneralLayout.vue
+++ resources/js/Layouts/GeneralLayout.vue
@@ -1,13 +1,14 @@
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
+import image from '../../../public/storage/logo_img/travel-app-logo.png';
 </script>
 
 <template>
     <HeaderComponent />
     <main>
         <slot />
-        <FooterComponent />
+        <FooterComponent :logo="image" />
     </main>
 </template>
 
