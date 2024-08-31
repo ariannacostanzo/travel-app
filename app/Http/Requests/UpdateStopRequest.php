abd7bf60a4da225b4ee9fd315c7dee394fa9b931
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-08-31 14:33:05 +0200

    added update stop validation

---
+++ app/Http/Requests/UpdateStopRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateStopRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+
+        return [
+            'title' => 'required|string',
+            'image' => 'nullable|string',
+            'foods' => 'nullable|string',
+            'is_completed' => 'boolean',
+            'address' => 'required|string',
+            'latitude' => 'required',
+            'longitude' => 'required',
+        ];
+    }
+}
